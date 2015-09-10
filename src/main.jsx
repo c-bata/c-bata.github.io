90cda2b00fa982b8d19da8dcf0721277118100f8
c-bata <contact@c-bata.link>
2015-09-11 00:52:18 +0900

    Render string variable on React

---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import React  from 'react';
+import marked from 'marked';
+
+var defaultText = `# Masashi Shibata
+
+a.k.a id: c-bata or c_bata_
+
+- Twitter: [@c_bata_](http://twitter.com/c_bata_)
+- Facebook: [masashi.cbata](http://facebook.com/masashi.cbata)
+- Github: [c-bata](http://github.com/c-bata)
+- BitBucket: [c-bata](https://bitbucket.org/c-bata/)
+- Email: contact (at) c-bata.link
+
+Please feel free to contact me !
+`
+
+class MarkdownPreview extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: defaultText,
+        };
+    }
+    render() {
+        let rawHtml = marked(this.state.text)
+        return (
+            <div className="editorBox">
+                <span dangerouslySetInnerHTML={{__html: rawHtml}} />
+            </div>
+        );
+    }
+};
+
+React.render(
+    <MarkdownPreview />,
+    document.getElementById('content')
+);
+
