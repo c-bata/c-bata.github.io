ac114837a7d42c4e83ecf0f68d1ce6f21c57cdf1
c-bata <contact@c-bata.link>
2015-09-11 03:23:26 +0900

    Refactor js code

--- src/main.js
+++ src/main.js
@@ -7,27 +7,29 @@ import article_research    from '../markdown/research';
 
 let content_area       = document.getElementById('content');
 
-let buttons = {
-  LINKS:    document.getElementById('links'),
-  ABOUT:    document.getElementById('about'),
-  DEVELOP:  document.getElementById('development'),
-  RESEARCH: document.getElementById('research')
-};
+let pages = [
+    {
+        button: document.getElementById('links'),
+        content: article_links
+    },
+    {
+        button: document.getElementById('about'),
+        content: article_about
+    },
+    {
+        button: document.getElementById('development'),
+        content: article_development
+    },
+    {
+        button: document.getElementById('research'),
+        content: article_research
+    }
+];
 
-buttons.LINKS.addEventListener("click", () => {
-    content_area.innerHTML = marked(article_links);
-});
-
-buttons.ABOUT.addEventListener("click", () => {
-    content_area.innerHTML = marked(article_about);
-});
-
-buttons.DEVELOP.addEventListener("click", () => {
-    content_area.innerHTML = marked(article_development);
-});
-
-buttons.RESEARCH.addEventListener("click", () => {
-    content_area.innerHTML = marked(article_research);
+pages.map((page) => {
+    page.button.addEventListener("click", () => {
+        content_area.innerHTML = marked(page.content);
+    });
 });
 
 content_area.innerHTML = marked(article_links);
