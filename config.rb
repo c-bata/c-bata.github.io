42d90d88225527d7a1253c94b0d4599a644695b9
c-bata <contact@c-bata.link>
2015-09-11 18:35:48 +0900

    Add compass setting

--- config.rb
+++ config.rb
@@ -8,8 +8,11 @@ sass_dir = "src/scss"
 images_dir = "img"
 javascripts_dir = "javascripts"
 
-# You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
+
+# sourcemap
+sourcemap = true
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
