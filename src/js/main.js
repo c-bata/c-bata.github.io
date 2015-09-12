d8b550ed043a1386d121903a24d3fd2c15c36080
c-bata <contact@c-bata.link>
2015-09-12 12:12:53 +0900

    Implement dynamic sidebar with d3

--- src/js/main.js
+++ src/js/main.js
@@ -7,19 +7,29 @@ import article_develop  from '../../markdown/development';
 import article_research from '../../markdown/research';
 
 let pages = [
-    { content: article_links,    button: document.getElementById('links')       },
-    { content: article_about,    button: document.getElementById('about')       },
-    { content: article_develop,  button: document.getElementById('development') },
-    { content: article_research, button: document.getElementById('research')    }
+    { content: article_links,    buttonId: 'links',       label: "リンク" },
+    { content: article_about,    buttonId: 'about',       label: "About" },
+    { content: article_develop,  buttonId: 'development', label: "スキル" },
+    { content: article_research, buttonId: 'research',    label: "研究"   }
 ];
 
+// sidebar
+let pageList = d3.select("#sidebar").append("ul").selectAll("li");
+let updatePageList = pageList.data(pages); /* 対応する要素が足りる場合 -> text */
+let enterPageList = updatePageList.enter(); /* 対応する要素が足りない場合 -> append */
+
+enterPageList.append("li").html((page) => {
+    return `<a href="javascript: void(0)" id="${ page.buttonId }">${ page.label }</a>`;
+});
+
+// contents
 let content_area = d3.select("#content");
 
 pages.map((page) => {
-    page.button.addEventListener("click", () => {
+    let button = document.getElementById(page.buttonId);
+    button.addEventListener("click", () => {
         content_area.html(marked(page.content));
     });
 });
 
 content_area.html(marked(article_links));
-
