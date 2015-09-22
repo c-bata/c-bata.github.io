00f92185582720cc9d221639fd2e41c95f1db43f
c-bata <contact@c-bata.link>
2015-09-23 02:55:06 +0900

    Modify contents

--- markdown/about.js
+++ markdown/about.js
@@ -1,23 +1,43 @@
 export default `
-## 自己紹介
+## Welcome!
 
 芝田 将 (Masashi Shibata)
 
-DjangoやFlaskでWebアプリを開発しつつ，研究ではpandasなどPythonを積極的に使ってます。
-出来るだけ作ったものや勉強した内容は公開することを大事にしています。
+- [Github: c-bata](http://github.com/c-bata)
+- [Twitter: \`@c_bata_\`](http://twitter.com/c_bata_)
+- [Facebook: masashi.cbata](http://facebook.com/masashi.cbata)
+- [Hatena blog: c-bata web](http://nwpct1.hatenablog.com)
+- [Wantedly: 芝田 将](https://www.wantedly.com/users/3087528)
+- [Slideshare: c-bata](http://www.slideshare.net/c-bata)
+- [BitBucket: c-bata](https://bitbucket.org/c-bata/)
+- Email: contact (at) c-bata.link
+
+Please feel free to contact me !
+
 
 ## 所属
 
-- 明石高専 専攻科 1年(2017年度卒業予定)
+- 明石高専 専攻科 1年
 - BeProud アルバイト
 - PyCon JP 2015 メディア・スタッフ
 - akashi.py (Python勉強会) 主催
+- Code for Kosen
+
+
+## 学歴・職歴
+
+- 国立明石工業高等専門学校 機械・電子システム工学専攻 (2015.04 - 2017.03 卒業見込み)
+- 国立明石工業高等専門学校 電気情報工学科 (2010.04 - 2015.03)
+
+## アルバイト・インターン
+
+- BePROUD part-time (2015.03 - now)
+- Teamlab Internship(2015.09 - now)
+- CyberAgent Internship(2015.08 - 2015.09)
 
 
 ## 執筆歴
 
-- [2014.09 PIN入力認証の実装における一考察 (MEとバイオサイバネティックス)](http://ci.nii.ac.jp/naid/40020225048)
-- [2014.11 スマートフォンにおけるPIN入力認証における閾値決定手法](http://www.ieice.org/ess/biox/sbra2014/)
 - [2015.06 台湾で開催された「PyCon APAC 2015」参加レポート | gihyo.jp 技術評論社](http://gihyo.jp/news/report/01/pycon-apac-2015)
 - [2015.08 PyCon JP 2015の作り方 | gihyo.jp 技術評論社](http://gihyo.jp/dev/serial/01/pycon-jp-2015)
 - [2015.09 PyCon JP 2015 開催前レポート | CodeZine](http://codezine.jp/article/detail/8923)
@@ -30,8 +50,11 @@ DjangoやFlaskでWebアプリを開発しつつ，研究ではpandasなどPython
 - 2015.09 CyberAgent TechDesignCAMPエンタメ編 優勝
 
 
-## 資格
+## 研究・論文
 
-- TOEIC 620
+スマートフォンにおけるPIN入力タッチスクリーンバイオメトリクス
+
+- [2014.09 PIN入力認証の実装における一考察 (MEとバイオサイバネティックス)](http://ci.nii.ac.jp/naid/40020225048)
+- [2014.11 スマートフォンにおけるPIN入力認証における閾値決定手法](http://www.ieice.org/ess/biox/sbra2014/)
+
 `
-
--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,12 @@
 import marked from 'marked';
 import d3     from 'd3';
 
-import article_links    from '../../markdown/links';
 import article_about    from '../../markdown/about';
 import article_develop  from '../../markdown/development';
-import article_research from '../../markdown/research';
 
 let pages = [
-    { content: article_links,    buttonId: 'links',       label: "トップ" },
-    { content: article_about,    buttonId: 'about',       label: "自己紹介" },
+    { content: article_about,    buttonId: 'about',       label: "トップ" },
     { content: article_develop,  buttonId: 'development', label: "スキル" },
-    { content: article_research, buttonId: 'research',    label: "研究"   }
 ];
 
 // sidebar
@@ -32,4 +28,4 @@ pages.map((page) => {
     });
 });
 
-content_area.html(marked(article_links));
+content_area.html(marked(article_about));
