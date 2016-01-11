b73f1064a0ae2978241134a88570116966d829ef
c-bata <contact@c-bata.link>
2016-01-11 15:07:41 +0900

    Update about.js

--- contents/about.js
+++ contents/about.js
@@ -10,9 +10,8 @@ export default `
     <li><a href="http://nwpct1.hatenablog.com">Hatena Blog</a></li>
     <li><a href="https://www.wantedly.com/users/3087528">Wantedly</a></li>
     <li><a href="http://www.slideshare.net/c-bata">Slideshare</a></li>
+    <li>Email: contact at c-bata.link</li>
 </ul>
-
-- Email: contact (at) c-bata.link
 
 <p>Please feel free to contact me !</p>
 
@@ -47,9 +46,28 @@ export default `
 <h2>執筆歴</h2>
 
 <ul>
-    <li><a href="http://gihyo.jp/news/report/01/pycon-apac-2015">2015.06 台湾で開催された「PyCon APAC 2015」参加レポート | gihyo.jp 技術評論社</a></li>
-    <li><a href="http://gihyo.jp/dev/serial/01/pycon-jp-2015">2015.08 PyCon JP 2015の作り方 | gihyo.jp 技術評論社</a></li>
-    <li><a href="http://codezine.jp/article/detail/8923">2015.09 PyCon JP 2015 開催前レポート | CodeZine</a></li>
+    <li><a href="http://gihyo.jp/news/report/01/pycon-apac-2015">台湾で開催された「PyCon APAC 2015」参加レポート | gihyo.jp 技術評論社</a></li>
+    <ul>
+        <li><a href="http://gihyo.jp/news/report/01/pycon-apac-2015/0001">2015.06 第1回　現地の様子とカンファレンス1日目 ～Van Lindberg氏による基調講演，おやつタイム，等々～</a></li>
+        <li><a href="http://gihyo.jp/news/report/01/pycon-apac-2015/0002">2015.06 第2回　PyCon APAC/Taiwan 2015カンファレンス2日目参加レポート ～Robert Bradshaw氏による基調講演，LT，等々～</a></li>
+        <li><a href="http://gihyo.jp/news/report/01/pycon-apac-2015/0003">2015.07 第3回（最終回）　PyCon APAC/Taiwan 2015カンファレンス3日目参加レポート ～プレゼント大会，等々～</a></li>
+    </ul>
+    <li><a href="http://gihyo.jp/dev/serial/01/pycon-jp-2015">PyCon JP 2015の作り方 | gihyo.jp 技術評論社</a></li>
+    <ul>
+        <li><a href="http://gihyo.jp/dev/serial/01/pycon-jp-2015/0003">2015.08 第3回　プログラムチーム編：テーマ，プログラム，新企画等</a></li>
+        <li><a href="http://gihyo.jp/dev/serial/01/pycon-jp-2015/0004">2015.09 第4回（最終回）　会場チーム編：会場，ネットワーク，ランチ，おやつ等</a></li>
+    </ul>
+    <li>PyCon JP 2015 開催前レポート | CodeZine</li>
+    <ul>
+        <li><a href="http://codezine.jp/article/detail/8923">2015.09 第1回 開催概要・キーノート</a></li>
+        <li><a href="http://codezine.jp/article/detail/8990">2015.09 第2回 プログラムについて</a></li>
+        <li><a href="http://codezine.jp/article/detail/8991">2015.10 第3回 会場・パーティについて</a></li>
+        <li><a href="http://codezine.jp/article/detail/9006">2015.10 第4回 その他の見どころ</a></li>
+    </ul>
+    <li>PyCon JP 2015 振り返りレポート | Codezine</li>
+    <ul>
+        <li><a href="http://codezine.jp/article/detail/9079">2015.11 ～第1回 座長あいさつ・メディアチームの作業内容</a></li>
+    </ul>
 </ul>
 
 
